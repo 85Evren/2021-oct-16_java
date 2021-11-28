d837916e2ce0340462f7138e39e76d112f863445
Evren Kasap <evrkll9@gmail.com>
2021-11-28 20:27:47 +0300

    Deneme

---
+++ test1/src/test1/test.java
@@ -0,0 +1,23 @@
+package test1;
+
+
+import java.util.*;
+import java.util.Arrays;
+
+
+public class test {
+	public static void main(String [] args)
+	{
+		
+		
+		int [] b = {1, -6, 5, 0 , 6};
+		int [] a = {1, -6,};
+		int sonuc = Arrays.compare(a, b);
+
+		System.out.println(sonuc);
+		
+		System.currentTimeMillis();
+	}
+	
+
+}
